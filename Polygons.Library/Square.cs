c736c021a25a1621aa72faf894b933d75b491dc2
Pranay Neema <neema.pranay23@gmail.com>
2020-06-14 10:51:05 +0530

    Add Square class

---
+++ Polygons.Library/Square.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygons.Library
+{
+    class Square : ConcreteRegularPolygon
+    {
+        public Square(int length) : base(4,length)
+        {
+
+        }
+
+        public override double GetArea()
+        {
+            return SideLength * SideLength;
+        }
+    }
+}
